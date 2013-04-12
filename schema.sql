9e2cb9a7b74ef1b45c5ccc5fc1b79e4483430f81
Gabriel Alonso <gbr.alonso@gmail.com>
2013-04-11 21:36:48 -0300

    Changes on schema.sql

--- schema.sql
+++ schema.sql
@@ -15,5 +15,5 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `user` WRITE;
-INSERT INTO `user` VALUES (1,'admin','BFEQkknI/c+Nd7BaG7AaiyTfUFby/pkMHy3UsYqKqDcmvHoPRX/ame9TnVuOV2GrBH0JK9g4koW+CgTYI9mK+w==','admin@intraway.com','Juan','Perez','ROLE_ADMIN');
+INSERT INTO `user` VALUES (1,'admin','BFEQkknI/c+Nd7BaG7AaiyTfUFby/pkMHy3UsYqKqDcmvHoPRX/ame9TnVuOV2GrBH0JK9g4koW+CgTYI9mK+w==','admin@test.com','Juan','Perez','ROLE_ADMIN');
 UNLOCK TABLES;
